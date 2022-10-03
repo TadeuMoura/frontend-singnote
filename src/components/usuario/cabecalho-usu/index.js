545c536860ec7bbdacbe3e3ddf597b9a0bddb268
Tadeu <tadeuhmc@hotemail.com>
2022-10-03 11:51:49 -0300

    aba lateral e cabecalho

---
+++ src/components/usuario/cabecalho-usu/index.js
@@ -0,0 +1,14 @@
+import './index.scss'
+
+export default function CabecalhoUSU() {
+    
+    return(
+    <div className="cabecalho-usu">  {/*Começo cabeçalho*/}
+            <div className='imagem-nome'>
+                <img src="/image/logo-sing.png"/>
+                <p className="titulo-sing"> Sing Note</p>
+            </div>
+            <div className="inicial-nome-usu"> T </div>
+        </div> /*Fim do cabeçalho*/
+
+)}
--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import ConsultaAdmin from "./pages/admin/ConsultaAdmin";
 import CadastrarProduto from "./pages/admin/CadastroProduto/index.js";
 import PageHome from "./pages/admin/home/index.js";
 import PagePedidos from "./pages/admin/pedidos/index.js"
+import AbaLateralADM from "./components/adm/aba-lateral-adm";
+import CabecalhoUSU from "./components/usuario/cabecalho-usu";
+import AbaLateralUSU from "./components/usuario/aba-lateral-usu";
 
 
 
@@ -19,6 +22,8 @@ root.render(
                 <Route path="/" element={<  CadastrarProduto/>} />
                 <Route path="/admin/home" element={< PageHome />} />
                 <Route path="/admin/pedidos" element={< PagePedidos />} />
+                <Route path="/abausu" element={< AbaLateralUSU />} />
+                <Route path="/cabeusu" element={< CabecalhoUSU />} />
             </Routes>
         </BrowserRouter>
   </React.StrictMode>
