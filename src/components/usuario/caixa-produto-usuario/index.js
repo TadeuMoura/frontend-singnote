fed10933dd59a54e717032563819d008acfef209
Tadeu Henrique Moura Carvalho <tadeuhmc@hotmail.com>
2022-10-03 23:25:50 -0300

    pagina debusca faltam as bolas

--- src/components/usuario/caixa-produto-usuario/index.js
+++ src/components/usuario/caixa-produto-usuario/index.js
@@ -5,15 +5,16 @@ export default function CaixaProdutoUsuario () {
 
     return(
         <main className='page-produto'>
-            <div className='id-imagens'>
-                <p>1#</p>
-                <div>
-                    <button><img src='/image/lapis.png' alt='lapis'></img></button>
-                    <button><img src='/image/lixo.png' alt=''></img></button>
-                </div>
-            </div>
             <div className='espaco-produto'>
-                    
+                <img className='imagem-coracao' src='/image/coracao-card.png' alt='coracao-do-card'/>
+                <div className='descricao-card'>
+                    <img className='imagem-produto-card' src='/image/bateria.png' alt='produto-do-card'/>
+                    <h1 className='card-produto-descricao'>Bateria Tama Starclassic Performer MBS42RZS CAR 4PC</h1>
+                    <h1 className='preco-card'>R$ 13.224,34</h1>
+                    <button className='botao-comprar'>Comprar
+                    <img className='imagem-carrinho' src='/image/carrinho-card.png' alt='carrinho-do-card'/>
+                    </button>
+                </div>
             </div>
         </main>
     )
--- src/pages/usuario/busca/index.js
+++ src/pages/usuario/busca/index.js
@@ -11,20 +11,58 @@ export default function BuscaUsuario () {
                 <div className='elemento-cabecalho'>
                     <CabecalhoUSU/>
                 </div>
-                <div/>
-                    <div className='elementos-pesquisa-usu'>  
-                        <input className='input-pesquisa-usu' type="search" placeholder='Buscar por nome' ></input>
-                        <button className='btn-buscar'>Buscar</button>
+                <div className='pesquisa-e-busca'>
+                    <input className='input-pesquisa-usu' type="search" placeholder='Buscar por nome' ></input>
+                    <button className='btn-buscar'>Buscar</button>
+                </div>
+                <div className='area-da-consulta-usu'>
+                    <div className='categoria-e-tipo'>
+                        <h1 className='titulo-categoria'>Categorias</h1>
+                        <div className='opcao-tipo-categoria'>
+                            <p className='cada-titulo'>Cordas</p>
+                            <p className='cada-titulo'>Sopro</p>
+                            <p className='cada-titulo'>Percurção</p>
+                            <p className='cada-titulo'>Elétricos</p>
+                            <p className='cada-titulo'>Teclas</p>
+                            <p className='cada-titulo'>Acessórios</p>
+                        </div>
+                        
+                        <h1 className='titulo-tipo'>Tipos</h1>
+                        <div className='opcao-tipo-categoria'>
+                            <p className='cada-titulo'>Violão</p>
+                            <p className='cada-titulo'>Guitarra</p>
+                            <p className='cada-titulo'>Piano</p>
+                            <p className='cada-titulo'>Teclado</p>
+                            <p className='cada-titulo'>Bateria</p>
+                            <p className='cada-titulo'>Pandeiro</p>
+                            <p className='cada-titulo'>Rebolo</p>
+                            <p className='cada-titulo'>Violino</p>
+                            <p className='cada-titulo'>Violancelo</p>
+                            <p className='cada-titulo'>Saxofone</p>
+                            <p className='cada-titulo'>Flauta</p>
+                        </div>
+
                     </div>
-                    <div className='cards-produto-usu'>
-                        <CaixaProdutoUsuario/>
-                        <CaixaProdutoUsuario/>
-                        <CaixaProdutoUsuario/>
-                        <CaixaProdutoUsuario/>
-                        <CaixaProdutoUsuario/>
-                        <CaixaProdutoUsuario/>
-                        <CaixaProdutoUsuario/>
-                        <CaixaProdutoUsuario/>
+                    <div className='parte-da-consulta'>
+                        <div className='resultados'> 
+                            <p>Resultado de busca:
+                            <span className='resultado-valor'> Bateria</span></p>
+                            <p>produtos: 
+                            <span className='resultado-valor'> 10</span></p></div>
+                        <div className='elementos-pesquisa-usu'>  
+                            
+                        </div>
+                            <div className='cards-produto-usu'>
+                                <CaixaProdutoUsuario/>
+                                <CaixaProdutoUsuario/>
+                                <CaixaProdutoUsuario/>
+                                <CaixaProdutoUsuario/>
+                                <CaixaProdutoUsuario/>
+                                <CaixaProdutoUsuario/>
+                                <CaixaProdutoUsuario/>
+                                <CaixaProdutoUsuario/>
+                            </div>
+                        </div>
                     </div>
             </div>
             
